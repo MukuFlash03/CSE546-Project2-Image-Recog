f3243606db38f4ef66ea0c5e8bb4f9b4ac11aa91
sa171 <rudrawars@gmail.com>
2023-04-21 18:48:46 -0700

    @sa171 added script for openstack

---
+++ ExternalService/app.py
@@ -0,0 +1,34 @@
+import boto3
+
+session = boto3.Session()
+sqs = session.client('sqs')
+lambda_client = session.client('lambda')
+s3 = session.client('s3')
+
+queue_name = "p3-sqs-1"
+queue_url = "https://sqs.us-east-1.amazonaws.com/874290406143/p3-sqs-1"
+lambda_function_name = "fae_recognition"
+
+while True:
+    try:
+        response = sqs.receive_message(
+            QueueUrl=queue_url,
+            WaitTimeSeconds=10,
+            VisibilityTimeout=2
+        )
+        if 'Messages' in response:
+            for message in response['Messages']:
+                body = message['Body']
+                lambda_client.invoke(
+                    FunctionName=lambda_function_name,
+                    InvocationType='Event',
+                    Payload=body
+                )
+                sqs.delete_message(
+                    QueueUrl=queue_url,
+                    ReceiptHandle=message['ReceiptHandle']
+                )
+    except Exception as exp:
+        print("Some error occured while monitoring SQS: ",exp)
+
+
