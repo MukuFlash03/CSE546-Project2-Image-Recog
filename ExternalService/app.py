0639e994a8495f45c9771ec908707b83e24e74b3
sa171 <rudrawars@gmail.com>
2023-04-25 23:39:45 -0700

    @sa171 modified script for printing output

--- ExternalService/app.py
+++ ExternalService/app.py
@@ -1,10 +1,12 @@
+import json
+
 import boto3
 
 session = boto3.Session()
 sqs = session.client('sqs')
 lambda_client = session.client('lambda')
-s3 = session.client('s3')
-
+s3 = boto3.resource('s3')
+bucket_name = 'output-bucket-results231'
 queue_name = "p3-sqs-1"
 queue_url = "https://sqs.us-east-1.amazonaws.com/874290406143/p3-sqs-1"
 lambda_function_name = "fae_recognition"
@@ -19,11 +21,20 @@ while True:
         if 'Messages' in response:
             for message in response['Messages']:
                 body = message['Body']
-                lambda_client.invoke(
-                    FunctionName=lambda_function_name,
-                    InvocationType='Event',
-                    Payload=body
-                )
+                records = json.loads(body)
+                for record in records['Records']:
+                    if record['s3']['bucket']['name'] == 'input-bucket-1523':
+                        lambda_client.invoke(
+                            FunctionName=lambda_function_name,
+                            InvocationType='Event',
+                            Payload=body
+                        )
+                    else:
+                        object_key = record['s3']['object']['key']
+                        bucket = s3.Bucket(bucket_name)
+                        result_object = bucket.Object(object_key)
+                        result = result_object.get()['Body'].read().decode('utf-8')
+                        print("Output for {} is {}".format(object_key,result))
                 sqs.delete_message(
                     QueueUrl=queue_url,
                     ReceiptHandle=message['ReceiptHandle']
